8db667f536c1b9461091afcd35eed7911a4b5a90
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-04-09 10:50:58 +0100

    JS advanced and some cSS corrected

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 function checkTable() {
-  
     var input = document.getElementById("userRow");
     var userColumn = document.getElementById("userColumn");
     var answer;
     var sOption = document.getElementById("sOption").value;
-    
-    if (input.value != "" && input.value > 0) {
-        errorMsg.innerHTML = ""
-        table.innerHTML = "";
-
-        if (sOption == "Multiplication") {
-            for (var j = 1; j <= userColumn.value; j++) {
-
-                `<tr>
-                            
-                </tr>`
-
-                for (var i = 1; i  <= input.value; i++) {
-                    
-                    answer = i * j ;
-                    table.innerHTML += 
-                    `
-                        <td> ${j} * ${i} = ${answer} </td>
-                    `
-                }
-
-            }
-        } 
-        
+    var tableHTML = "";
+  
+    if (input.value != "" && input.value > 0 && userColumn.value != "" && userColumn.value > 0) {
+      errorMsg.innerHTML = "";
+      table.innerHTML = "";
+  
+      if (sOption == "Multiplication") {
+        for (var i = 1; i <= userColumn.value; i++) {
+          tableHTML += "<tr>";
+  
+          for (var j = 1; j <= input.value; j++) {
+            answer = i * j;
+            tableHTML += `<td>${i} * ${j} = ${answer}</td>`;
+          }
+  
+          tableHTML += "</tr>";
+        }
+  
+        table.innerHTML = tableHTML;
+      }
+    } else {
+      table.innerHTML = "";
+      errorMsg.innerHTML = "If you are not a fool, you will know that empty input cant give answer";
     }
-            else {
-                table.innerHTML = "";
-        
-                errorMsg.innerHTML = "If you are not a fool, you will know that empty input cant give answer"
-            }
-
-    }
+  }
+  
