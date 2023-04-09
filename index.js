d41019e17c5ff72580f9c8a3594f90dbb3cb2d24
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-04-09 10:56:35 +0100

    JS advanced corrected

--- index.js
+++ index.js
@@ -22,7 +22,60 @@ function checkTable() {
         }
   
         table.innerHTML = tableHTML;
+      } else if (sOption == "Addition") {
+        for (var i = 1; i <= userColumn.value; i++) {
+          tableHTML += "<tr>";
+  
+          for (var j = 1; j <= input.value; j++) {
+            answer = i + j;
+            tableHTML += `<td>${i} + ${j} = ${answer}</td>`;
+          }
+  
+          tableHTML += "</tr>";
+        }
+  
+        table.innerHTML = tableHTML;
+      } else if (sOption == "Division") {
+        for (var i = 1; i <= userColumn.value; i++) {
+          tableHTML += "<tr>";
+  
+          for (var j = 1; j <= input.value; j++) {
+            answer = i / j;
+            tableHTML += `<td>${i} / ${j} = ${answer}</td>`;
+          }
+  
+          tableHTML += "</tr>";
+        }
+  
+        table.innerHTML = tableHTML;
+      } else if (sOption == "Subtraction") {
+        for (var i = 1; i <= userColumn.value; i++) {
+          tableHTML += "<tr>";
+  
+          for (var j = 1; j <= input.value; j++) {
+            answer = i - j;
+            tableHTML += `<td>${i} - ${j} = ${answer}</td>`;
+          }
+  
+          tableHTML += "</tr>";
+        }
+  
+        table.innerHTML = tableHTML;
+      } else {
+        for (var i = 1; i <= userColumn.value; i++) {
+          tableHTML += "<tr>";
+  
+          for (var j = 1; j <= input.value; j++) {
+            answer = i % j;
+            tableHTML += `<td>${i} mod ${j} = ${answer}</td>`;
+          }
+  
+          tableHTML += "</tr>";
+        }
+  
+        table.innerHTML = tableHTML;
       }
+
     } else {
       table.innerHTML = "";
       errorMsg.innerHTML = "If you are not a fool, you will know that empty input cant give answer";
